be007de0107950fd6910bb6e9d9505fb0001603a
rollwagen <rollwagen@gmail.com>
2020-08-22 14:20:51 +0000

    Initial commit.

---
+++ gitconfig.sh
@@ -0,0 +1,17 @@
+
+#git config --global user.email ""
+git config --global user.name "rollwagen"
+#git config user.email ""
+git config user.name "rollwagen"
+
+git config --global core.editor "vi"
+
+git config --global credential.helper 'cache --timeout 3600'
+
+
+#
+# show email and name
+#
+#git config user.email
+#git config user.name
+
