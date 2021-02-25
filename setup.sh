2f813e91fc48ee343ecc22795bf0fb192b36a7e4
rollwagen <rollwagen@gmail.com>
2021-02-25 11:53:31 +0000

    Added oh-my-zsh.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Run upgrade...
 sudo apt-get update; sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade;
 
-sudo apt install -y rsync nmap azure-cli zsh
+sudo apt install -y rsync nmap awscli zsh fzf
 
 sudo apt autoremove -y
 
@@ -11,4 +11,11 @@ source ~/.bash_profile
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
 source ~/.git-completion.bash
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# YELLOW="\[$(tput setaf 3)\]"
+# RESET="\[$(tput sgr0)\]"
+# PS1="\h:\W \u\$(__git_ps1 \" ${YELLOW}(%s)${RESET} \")\$ "
 
