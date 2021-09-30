54cce400984b9297a2adc52108f1a89de36e5891
rollwagen <rollwagen@gmail.com>
2021-09-30 17:57:51 +0200

    Fix shellcheck findings.

--- creds.sh
+++ creds.sh
@@ -1,1 +1,3 @@
-gpg --decrypt ~/aws_credentials.sh.gpg > ~/_awscreds.sh; source ~/_awscreds.sh; rm ~/_awscreds.sh;  echo RELOADAGENT | gpg-connect-agent
+#!/bin/bash
+# shellcheck source=/dev/null
+gpg --decrypt ~/aws_credentials.sh.gpg > ~/_awscreds.sh; . ~/_awscreds.sh; rm ~/_awscreds.sh;  echo RELOADAGENT | gpg-connect-agent
