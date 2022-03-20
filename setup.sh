92cab1115ad34b57b18142232a4434c481b860f2
rollwagen <rollwagen@gmail.com>
2022-03-20 13:32:04 +0000

    Also install pre-commit and golangci-lint

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 #sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade;
 
-sudo apt install -y jq rsync nmap "zsh" fzf zsh-syntax-highlighting neovim netcat shellcheck fd-find
+sudo apt install -y jq rsync nmap "zsh" fzf zsh-syntax-highlighting neovim netcat shellcheck fd-find pre-commit
 sudo apt autoremove -y
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
@@ -35,7 +35,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 # RESET="\[$(tput sgr0)\]"
 # PS1="\h:\W \u\$(__git_ps1 \" ${YELLOW}(%s)${RESET} \")\$ "
 
-alias zsh=/usr/bin/zsh
+alias zsh=/bin/zsh
 alias ls=lsd
 alias cat=bat
 alias vi=nvim
@@ -44,3 +44,20 @@ alias vi=nvim
 sudo apt-get install -y locales
 sudo locale-gen "en_US.UTF-8"
 sudo update-locale LC_ALL="en_US.UTF-8"
+
+go install golang.org/x/tools/gopls@latest
+
+# ripgrep
+sudo apt-get install ripgrep
+
+#
+# neovim
+#
+# https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install -y neovim
+
+# golangci-lint install
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.45.0
+
